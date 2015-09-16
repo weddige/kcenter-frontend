983553ea10fa38d66ddd2c45013141975a483c2b
Konstantin Weddige <konstantin@skathi.net>
2015-09-16 15:21:55 +0200

    Change style to a more TUMy appearance

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
-angular.module('kcenter', [])
-.controller('KCenterController', ['$http', function($http) {
+angular.module('kcenter', ['kcenter.config'])
+.controller('KCenterController', ['$http', '$timeout', 'COMPUTATION_SERVER', function($http, $timeout, COMPUTATION_SERVER) {
     this.params = [];
     this.options = [];
     this.param = '';
-    this.computation_server = 'http://localhost:8887';
+    this.tasks = [];
+    this.computation_server = COMPUTATION_SERVER;
+    //this.computation_server = 'http://m09vm9.ma.tum.de:8887';
 
     this.state = 'select';
 
@@ -21,7 +23,7 @@ angular.module('kcenter', [])
                 self.init();
                 if (status == 202) {
                     self.state = 'waiting';
-                    self.wait(this.computation_server + headers('Location'));
+                    self.wait(self.computation_server + headers('Location'));
                 } else {
                     self.state = 'view';
                     self.data = data;
@@ -36,18 +38,30 @@ angular.module('kcenter', [])
             if (data['state'] == 'finished') {
 			    self.state = 'view';
                 self.data = data;
+            } else if (data['state'] == 'failed') {
+                self.state = 'error';
 			} else {
 			    setTimeout(function(){ self.wait(url); }, 1000);
 		    }
         });
     }
 
-    this.init = function init(self) {
+    this.examples = function() {
+        var self = this;
+        var url = this.computation_server + '/tasks';
+        $http.get(url).success(function(data) {
+            self.tasks = data;
+        });
+        $timeout(function() { self.examples() }, 5000);
+    }
+
+    this.init = function init() {
         var self = this;
         var url = this.computation_server + '/algorithms';
         $http.get(url).success(function(data) {
             self.options = data['suggestions'];
         });
+        this.examples();
     }
 
     this.restart = function restart() {
@@ -56,33 +70,3 @@ angular.module('kcenter', [])
 
     this.init();
 }]);
-
-var computation_server = 'http://localhost:8887';
-
-
-function get_value(item) {
-	var result = item.getAttribute('value');
-	if (result == null) {
-		var selected = item.childNodes[item.selectedIndex];
-		if (selected)
-			result = selected.getAttribute('value')||selected.innerText;
-	}
-	return result;
-}
-
-function wait_for_result(uri) {
-	var request = new XMLHttpRequest();
-    request.open('GET', computation_server + uri);
-
-    request.onreadystatechange = function() {
-        if (request.readyState == 4) {
-            data = JSON.parse(request.responseText);
-            if (data['state'] == 'finished') {
-			    show_result(data);
-			} else {
-			    setTimeout(function(){ wait_for_result(uri); }, 1000);
-			}
-        }
-    }
-    request.send();
-}
